38dccd037126971249b1c9650418d19e673e82e1
Leticia Pinheiro <161384435+leticiapinheirods@users.noreply.github.com>
2025-03-11 17:38:57 -0300

    feat: terminando os métodos do jogo

--- jogo-da-velha.c
+++ jogo-da-velha.c
@@ -4,9 +4,10 @@
 #include <stdbool.h>
 
 char matriz[3][3], s='X';
-int nj = 1, op, i, j;
+int i, j, jogada=1, op, partidas = 0, empates = 0, vx = 0, vo = 0;
 
-void matriz_jogo(){
+void jogo(){
+    printf("\n");
     printf("        *           *        \n");
     printf("  %c    *    %c     *   %c   \n", matriz[0][0], matriz[0][1], matriz[0][2]);
     printf("        *           *        \n");
@@ -20,6 +21,16 @@ void matriz_jogo(){
     printf("        *           *        \n");
 }
 
+void estatisticas(){
+    printf("*         ESTATISTICAS        *\n");
+    printf("*******************************\n");
+    printf("*TOTAL DE PARTIDAS: %d        *\n", partidas);
+    printf("*VITÓRIAS JOGADOR X: %d       *\n", vx);
+    printf("*VITÓRIAS JOGADOR Y: %d       *\n", vo);
+    printf("*EMPATES: %d                  *\n", empates);
+    printf("*******************************\n");
+}
+
 void menu(){
     printf("*         GAME VELHA          *\n");
     printf("*******************************\n");
@@ -30,7 +41,7 @@ void menu(){
     printf("Selecione uma opção: ");
 }
 
-char coordenadas(char s){
+char coordenadas(){
     do{
         printf("Qual linha você quer posicionar %c (insira um numero de 1 a 3)? ", s);
         scanf("%d", &i);
@@ -44,7 +55,24 @@ char coordenadas(char s){
     return matriz[i-1][j-1];
 }
 
-//por que aqui se passar os parametros s, i, j da erro?
+int verificar_vitoria(){
+    for (int i = 0; i < 3; i++) {
+        if (matriz[i][0] == matriz[i][1] && matriz[i][1] == matriz[i][2]){
+            return matriz[i][0];
+        } else if(matriz[0][i] == matriz[1][i] && matriz[1][i] == matriz[2][i]){
+            return matriz[0][i];
+        }
+    }
+
+    if (matriz[0][0] == matriz[1][1] && matriz[1][1] == matriz[2][2]){
+        return matriz[0][0];
+    }
+         
+    if (matriz[0][2] == matriz[1][1] && matriz[1][1] == matriz[2][0]){
+        return matriz[0][2];
+    } 
+}
+
 void jogar(){
     do{
         system("clear");
@@ -55,30 +83,56 @@ void jogar(){
             s = 'X';
         }
         
-        coordenadas(s);
+        coordenadas();
         
         while(matriz[i-1][j-1]){
             printf("Coordenada não disponível. Insira outra\n");
             
-            coordenadas(s);
+            coordenadas();
         }
-
+        
         matriz[i-1][j-1] = s;
 
-        matriz_jogo();
+        jogo();
 
-        nj++;
+        jogada++;
+        
+        if(jogada > 4){
+            if(verificar_vitoria()){
+                char vencedor = verificar_vitoria();
+
+                if (vencedor == 'X'){
+                    printf("Vitória do Jogador X!\n");
+                    sleep(2);
+                    vx++;
+                    break;
+                } else if (vencedor == 'O'){
+                    printf("Vitória do Jogador O!\n");
+                    sleep(2);
+                    vo++;
+                    break;
+                } else{
+                    printf("O jogo empatou\n");
+                    sleep(2);
+                    empates++;
+                    break;
+                }
+            }
+        
+        }
 
         sleep(2);
 
-    }while(nj < 10);
+    }while(jogada < 10);
     
-    printf("Fim do jogo!");
+    partidas++;
+
+    printf("Fim do jogo!\n");
 }
 
-
-int main(){
+void main(){
     do{
+        system("clear");
         menu();
         scanf("%d", &op);
         system("clear");
@@ -87,11 +141,16 @@ int main(){
             case 1:
                 jogar();
                 break;
+            case 2:
+                estatisticas();
+                sleep(2);
+                break;
+            case 3:
+                break;
             default:
                 printf("Opção não disponível. Insira uma das opções do menu\n");
         }
     }while(op!=3);
 
     printf("Programa finalizado");
-    return 0;
 }
