78ca5374febb085d137f045fd43ba6bf6b93aada
Leticia Pinheiro <161384435+leticiapinheirods@users.noreply.github.com>
2025-03-18 18:24:05 -0300

    feat: permitir a inserção do nome dos jogadores

--- jogo-da-velha.c
+++ jogo-da-velha.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <stdbool.h>
 
-char matriz[3][3], s='X';
+char matriz[3][3], s='X', jx[50], jo[50];
 int i, j, jogada=1, op, partidas = 0, empates = 0, vx = 0, vo = 0;
 
 void jogo(){
@@ -25,8 +25,8 @@ void estatisticas(){
     printf("*         ESTATISTICAS        *\n");
     printf("*******************************\n");
     printf("*TOTAL DE PARTIDAS: %d        *\n", partidas);
-    printf("*VITÓRIAS JOGADOR X: %d       *\n", vx);
-    printf("*VITÓRIAS JOGADOR O: %d       *\n", vo);
+    printf("*VITÓRIAS JOGADOR(A) %s: %d       *\n", jx, vx);
+    printf("*VITÓRIAS JOGADOR(A) %s: %d       *\n", jo, vo);
     printf("*EMPATES: %d                  *\n", empates);
     printf("*******************************\n");
 }
@@ -41,7 +41,17 @@ void menu(){
     printf("Selecione uma opção: ");
 }
 
+void inicializar_matriz() {
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            matriz[i][j] = ' ';
+        }
+    }
+}
+
 char coordenadas(){
+    
+    //impedir que o jogador insira vazio
     do{
         printf("Qual linha você quer posicionar %c (insira um numero de 1 a 3)? ", s);
         scanf("%d", &i);
@@ -74,13 +84,27 @@ int verificar_vitoria(){
 }
 
 void jogar(){
+    
+    //e se o jogador quiser trocar de simbolo no meio do jogo?
+    //impedir que o jogador insira vazio
+    if(jx[0] == '\0' && jo[0] == '\0'){
+        
+        printf("Insira o nome do jogador(a) X: ");
+        scanf("%s", jx);
+    
+        printf("Insira o nome do jogador(a) O: ");
+        scanf("%s", jo);
+    }
+    
     do{
         system("clear");
 
         if (s == 'X'){
             s = 'O';
+            printf("Vez do jogador(a) %s", jo);
         } else{
             s = 'X';
+            printf("Vez do jogador(a) %s", jx);
         }
         
         jogo();
@@ -105,17 +129,17 @@ void jogar(){
                 jogo();
 
                 if (vencedor == 'X'){
-                    printf("Vitória do Jogador X!\n");
+                    printf("Vitória do jogador(a) %s!\n", jx);
                     sleep(2);
                     vx++;
                     break;
                 } else if (vencedor == 'O'){
-                    printf("Vitória do Jogador O!\n");
+                    printf("Vitória do jogador(a) %s!\n", jo);
                     sleep(2);
                     vo++;
                     break;
                 } else{
-                    printf("O jogo empatou\n");
+                    printf("Deu velha!\n");
                     sleep(2);
                     empates++;
                     break;
@@ -141,15 +165,14 @@ void main(){
         switch(op){
             case 1:
                 jogar();
-                
-                for(i=0; i<3; i++){
-                    for(j=0; j<3; j++){
-                        matriz[i][j] = NULL;
-                    }
-                }
-                
+                inicializar_matriz();
                 break;
             case 2:
+                if(jx[0] == '\0' && jo[0] == '\0'){
+                    printf("Necessário iniciar o jogo primeiro\n");
+                    sleep(2);
+                    break;
+                }
                 estatisticas();
                 sleep(2);
                 break;
@@ -158,6 +181,7 @@ void main(){
             default:
                 printf("Opção não disponível. Insira uma das opções do menu\n");
         }
+        
     }while(op!=3);
 
     printf("Programa finalizado");
